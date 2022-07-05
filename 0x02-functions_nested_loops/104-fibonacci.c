0311d7d9890f38cf18576526e1d0f57b4b42935d
Sophie77shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-07-05 19:14:24 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 /**
- * main - print first 50 fibonacci
- *
- * Return: 0 always
+ * main - finds and prints the first 98 fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	long int i, x = 1, y = 2, sum = 0, tSum = 0;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	for (i = 0; i < 49; i++)
+	j = 1;
+	k = 2;
 
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		if ((y % 2 == 0) && (y <= 4000000))
-		{
-			tSum = tSum + y;
-		}
-	sum = x + y;
-x = y;
-y = sum;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
-	printf("%ld\n", tSum);
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+	printf("\n");
+
 	return (0);
 }
