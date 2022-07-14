9fc1f6a7abafb0503ea334f82b8c89c9cf6c99af
Sophie77shalom <106449659+Sophie77shalom@users.noreply.github.com>
2022-07-14 08:06:59 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,45 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * _strncpy - two words
+ * @dest : pointer to char params
+ * @src : pointer to char params
+ * @n : int params
+ * Return: *dest
+ */
 
-
-int main(void)
-
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
 
-  int n;
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
 
-  int a[5];
-
-  int *p;
-
-
-
-  a[2] = 1024;
-
-  p = &n;
-
-  /*
-
-   * write your line of code here...
-
-   * Remember:
-
-   * - you are not allowed to use a
-
-   * - you are not allowed to modify p
-
-   * - only one statement
-
-   * - you are not allowed to code anything else than this line of code
-
-   */
-
- p[5] = 98;
-
-  /* ...so that this prints 98\n */
-
-  printf("a[2] = %d\n", a[2]);
-
-  return (0);
-
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
